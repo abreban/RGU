c37888fe7cda35f7064d86b5e366e2f65f7a942d
Andrei Breban <abreban@enermech.com>
2015-11-23 20:32:58 +0000

    try

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">SQL ''</div>
+                <div class="title">SQL 'li'</div>
             </div>
         </div>
     </body>
