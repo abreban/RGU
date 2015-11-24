2aba3e7df7fcc1dd9cd2f4b2c8c107420df7a815
bogdanstefan <b.s.hobinca@rgu.ac.uk>
2015-11-24 23:34:19 +0000

    changed loading type of script and css files

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,9 +5,9 @@
     <title>Wander Blog</title>
 
     <!--bootstrap css-->
-    <link type="application/css" rel="stylesheet" href="../dependencies/css/bootstrap/bootstrap.css" />
-    <link type="application/css" rel="stylesheet" href="../dependencies/css/bootstrap/bootstrap-theme.min.css" />
-    <link type="application/css" rel="stylesheet" href="../resources/styles/app.css" />
+    <link type="application/css" rel="stylesheet" href="{!! asset("dependencies/css/bootstrap/bootstrap.css")  !!}" />
+    <link type="application/css" rel="stylesheet" href="{!! asset("dependencies/css/bootstrap/bootstrap-theme.min.css")  !!}" />
+    <link type="application/css" rel="stylesheet" href="{!! asset("resources/styles/app.css")  !!}" />
 
 </head>
 <body ng-app="app" ng-cloak >
@@ -25,30 +25,30 @@
 
 
 <!--jquery-->
-<script type="application/javascript" src="../dependencies/scripts/jquery/jquery-1.11.3.js"></script>
+<script type="application/javascript" src="{!! asset("dependencies/scripts/jquery/jquery-1.11.3.js")  !!}"></script>
 
 <!--bootstrap js-->
-<script type="application/javascript" src="../dependencies/scripts/bootstrap/bootstrap.min.js"></script>
+<script type="application/javascript" src="{!! asset("dependencies/scripts/bootstrap/bootstrap.min.js")  !!}"></script>
 
 <!--angular-->
-<script type="application/javascript" src="../dependencies/scripts/angular/angular.min.js"></script>
-<script type="application/javascript" src="../dependencies/scripts/ui bootstrap/ui-bootstrap-tpls-0.14.3.min.js"></script>
-<script type="application/javascript" src="../dependencies/scripts/angular/angular-route.min.js"></script>
-<script type="application/javascript" src="../dependencies/scripts/angular/angular-resource.min.js"></script>
+<script type="application/javascript" src="{!! asset("dependencies/scripts/angular/angular.min.js")  !!}"></script>
+<script type="application/javascript" src="{!! asset("dependencies/scripts/ui bootstrap/ui-bootstrap-tpls-0.14.3.min.js")  !!}"></script>
+<script type="application/javascript" src="{!! asset("dependencies/scripts/angular/angular-route.min.js")  !!}"></script>
+<script type="application/javascript" src="{!! asset("dependencies/scripts/angular/angular-resource.min.js")  !!}"></script>
 
 <!--setup js-->
-<script type="application/javascript" src="../resources/scripts/js/setup.js"></script>
+<script type="application/javascript" src="{!! asset("resources/scripts/js/setup.js")  !!}"></script>
 
 <!--all js-->
-<script type="application/javascript" src="../resources/scripts/js/mainController.js"></script>
-<script type="application/javascript" src="../public/pages/home/home.js"></script>
-<script type="application/javascript" src="../public/pages/adventures/adventures.js"></script>
-<script type="application/javascript" src="../public/pages/adventures/adventureDetails.js"></script>
-<script type="application/javascript" src="../public/pages/adventures/addAdventure.js"></script>
-<script type="application/javascript" src="../public/pages/authors/authors.js"></script>
-<script type="application/javascript" src="../public/pages/authors/authorDetails.js"></script>
-<script type="application/javascript" src="../public/pages/admin/dashboard.js"></script>
-<script type="application/javascript" src="../public/pages/admin/roleManagement.js"></script>
-<script type="application/javascript" src="../public/pages/admin/userManagement.js"></script>
-<script type="application/javascript" src="../public/pages/about/about.js"></script>
+<script type="application/javascript" src="{!! asset("resources/scripts/js/mainController.js")  !!}"></script>
+<script type="application/javascript" src="{!! asset("pages/home/home.js")  !!}"></script>
+<script type="application/javascript" src="{!! asset("pages/adventures/adventures.js")  !!}"></script>
+<script type="application/javascript" src="{!! asset("pages/adventures/adventureDetails.js")  !!}"></script>
+<script type="application/javascript" src="{!! asset("pages/adventures/addAdventure.js")  !!}"></script>
+<script type="application/javascript" src="{!! asset("pages/authors/authors.js")  !!}"></script>
+<script type="application/javascript" src="{!! asset("pages/authors/authorDetails.js")  !!}"></script>
+<script type="application/javascript" src="{!! asset("pages/admin/dashboard.js")  !!}"></script>
+<script type="application/javascript" src="{!! asset("pages/admin/roleManagement.js")  !!}"></script>
+<script type="application/javascript" src="{!! asset("pages/admin/userManagement.js")  !!}"></script>
+<script type="application/javascript" src="{!! asset("pages/about/about.js")  !!}"></script>
 </html>
