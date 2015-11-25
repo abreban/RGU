940eb34ca94cc6aa5dfe0b8ff207e6dd713b18da
bogdanstefan <b.s.hobinca@rgu.ac.uk>
2015-11-25 00:21:54 +0000

    Fixed dependencies and routing

--- public/resources/scripts/js/setup.js
+++ public/resources/scripts/js/setup.js
@@ -13,17 +13,17 @@ app.config(function($interpolateProvider, $routeProvider){
     $interpolateProvider.startSymbol('[[').endSymbol(']]');
 
     $routeProvider
-        .when('/', {templateUrl: "../public/pages/home/home.html", controller: 'homeController'})
-        .when('/adventures', {templateUrl: "../public/pages/adventures/adventures.html", controller: 'adventuresController'})
-        .when('/addAdventure', {templateUrl: '../public/pages/adventures/addAdventure.html', controller: 'addAdventureController'})
-        .when('/adventureDetails', {templateUrl: '../public/pages/adventures/adventureDetails.html', controller: 'adventureDetailsController'})
-        .when('/authors',{templateUrl: '../public/pages/authors/authors.html', controller: 'authorsController'})
-        .when('/authorDetails',{templateUrl: '../public/pages/authors/authorDetails.html', controller: 'authorDetailsController'})
-        .when('/dashboard',{templateUrl: '../public/pages/admin/dashboard.html', controller: 'dashboardController'})
-        .when('/userManagement',{templateUrl: '../public/pages/admin/userManagement.html', controller: 'userManagementController'})
-        .when('/roleManagement', {templateUrl: '../public/pages/admin/roleManagement.html', controller: 'roleManagementController'})
-        .when('/about', {templateUrl: '../public/pages/about/about.html', controller: 'aboutController'})
-        .otherwise({ redirectTo: '../public/pages/home/home.html'});
+        .when('/', {templateUrl: "pages/home/home.html", controller: 'homeController'})
+        .when('/adventures', {templateUrl: "pages/adventures/adventures.html", controller: 'adventuresController'})
+        .when('/addAdventure', {templateUrl: 'pages/adventures/addAdventure.html', controller: 'addAdventureController'})
+        .when('/adventureDetails', {templateUrl: 'pages/adventures/adventureDetails.html', controller: 'adventureDetailsController'})
+        .when('/authors',{templateUrl: 'pages/authors/authors.html', controller: 'authorsController'})
+        .when('/authorDetails',{templateUrl: 'pages/authors/authorDetails.html', controller: 'authorDetailsController'})
+        .when('/dashboard',{templateUrl: 'pages/admin/dashboard.html', controller: 'dashboardController'})
+        .when('/userManagement',{templateUrl: 'pages/admin/userManagement.html', controller: 'userManagementController'})
+        .when('/roleManagement', {templateUrl: 'pages/admin/roleManagement.html', controller: 'roleManagementController'})
+        .when('/about', {templateUrl: 'pages/about/about.html', controller: 'aboutController'})
+        .otherwise({ redirectTo: 'pages/home/home.html'});
 });
 
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,50 +5,50 @@
     <title>Wander Blog</title>
 
     <!--bootstrap css-->
-    <link type="application/css" rel="stylesheet" href="{!! asset("dependencies/css/bootstrap/bootstrap.css")  !!}" />
-    <link type="application/css" rel="stylesheet" href="{!! asset("dependencies/css/bootstrap/bootstrap-theme.min.css")  !!}" />
-    <link type="application/css" rel="stylesheet" href="{!! asset("resources/styles/app.css")  !!}" />
+    <link type="application/css" rel="stylesheet" href="dependencies/css/bootstrap/bootstrap.css" />
+    <link type="application/css" rel="stylesheet" href="dependencies/css/bootstrap/bootstrap-theme.min.css" />
+    <link type="application/css" rel="stylesheet" href="resources/styles/app.css" />
 
 </head>
 <body ng-app="app" ng-cloak >
 <div class="main" ng-controller="mainController">
-    <div ng-include="'../public/partials/header.html'"></div>
-    <div ng-include="'../public/partials/menu.html'"></div>
+    <div ng-include="'partials/header.html'"></div>
+    <div ng-include="'partials/menu.html'"></div>
 
     <div class="container">
         <ng-view></ng-view>
     </div>
 
-    <div ng-include="'../public/partials/footer.html'"></div>
+    <div ng-include="'partials/footer.html'"></div>
 </div>
 </body>
 
 
 <!--jquery-->
-<script type="application/javascript" src="{!! asset("dependencies/scripts/jquery/jquery-1.11.3.js")  !!}"></script>
+<script type="application/javascript" src="dependencies/scripts/jquery/jquery-1.11.3.js"></script>
 
 <!--bootstrap js-->
-<script type="application/javascript" src="{!! asset("dependencies/scripts/bootstrap/bootstrap.min.js")  !!}"></script>
+<script type="application/javascript" src="dependencies/scripts/bootstrap/bootstrap.min.js"></script>
 
 <!--angular-->
-<script type="application/javascript" src="{!! asset("dependencies/scripts/angular/angular.min.js")  !!}"></script>
-<script type="application/javascript" src="{!! asset("dependencies/scripts/ui bootstrap/ui-bootstrap-tpls-0.14.3.min.js")  !!}"></script>
-<script type="application/javascript" src="{!! asset("dependencies/scripts/angular/angular-route.min.js")  !!}"></script>
-<script type="application/javascript" src="{!! asset("dependencies/scripts/angular/angular-resource.min.js")  !!}"></script>
+<script type="application/javascript" src="dependencies/scripts/angular/angular.min.js"></script>
+<script type="application/javascript" src="dependencies/scripts/ui bootstrap/ui-bootstrap-tpls-0.14.3.min.js"></script>
+<script type="application/javascript" src="dependencies/scripts/angular/angular-route.min.js"></script>
+<script type="application/javascript" src="dependencies/scripts/angular/angular-resource.min.js"></script>
 
 <!--setup js-->
-<script type="application/javascript" src="{!! asset("resources/scripts/js/setup.js")  !!}"></script>
+<script type="application/javascript" src="resources/scripts/js/setup.js"></script>
 
 <!--all js-->
-<script type="application/javascript" src="{!! asset("resources/scripts/js/mainController.js")  !!}"></script>
-<script type="application/javascript" src="{!! asset("pages/home/home.js")  !!}"></script>
-<script type="application/javascript" src="{!! asset("pages/adventures/adventures.js")  !!}"></script>
-<script type="application/javascript" src="{!! asset("pages/adventures/adventureDetails.js")  !!}"></script>
-<script type="application/javascript" src="{!! asset("pages/adventures/addAdventure.js")  !!}"></script>
-<script type="application/javascript" src="{!! asset("pages/authors/authors.js")  !!}"></script>
-<script type="application/javascript" src="{!! asset("pages/authors/authorDetails.js")  !!}"></script>
-<script type="application/javascript" src="{!! asset("pages/admin/dashboard.js")  !!}"></script>
-<script type="application/javascript" src="{!! asset("pages/admin/roleManagement.js")  !!}"></script>
-<script type="application/javascript" src="{!! asset("pages/admin/userManagement.js")  !!}"></script>
-<script type="application/javascript" src="{!! asset("pages/about/about.js")  !!}"></script>
+<script type="application/javascript" src="resources/scripts/js/mainController.js"></script>
+<script type="application/javascript" src="pages/home/home.js"></script>
+<script type="application/javascript" src="pages/adventures/adventures.js"></script>
+<script type="application/javascript" src="pages/adventures/adventureDetails.js"></script>
+<script type="application/javascript" src="pages/adventures/addAdventure.js"></script>
+<script type="application/javascript" src="pages/authors/authors.js"></script>
+<script type="application/javascript" src="pages/authors/authorDetails.js"></script>
+<script type="application/javascript" src="pages/admin/dashboard.js"></script>
+<script type="application/javascript" src="pages/admin/roleManagement.js"></script>
+<script type="application/javascript" src="pages/admin/userManagement.js"></script>
+<script type="application/javascript" src="pages/about/about.js"></script>
 </html>
